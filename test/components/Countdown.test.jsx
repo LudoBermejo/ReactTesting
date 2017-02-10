97611157352150f626371e5909859101ba0aa9d5
Ludo Bermejo <ludobermejo@gmail.com>
2017-02-10 12:58:33 +0100

    Add two seconds test

--- test/components/Countdown.test.jsx
+++ test/components/Countdown.test.jsx
@@ -34,13 +34,13 @@ describe('Countdown component', () => {
     it('should stop when reach 0', () => {
       jest.useFakeTimers();
       const countdown = shallow(<Countdown onStartCountdown={() => {}} />);
-      countdown.instance().startCountDown(1);
-      expect(countdown.state().totalSecondsToCount).toBe(1);
+      countdown.instance().startCountDown(2);
+      expect(countdown.state().totalSecondsToCount).toBe(2);
       expect(countdown.state().currentStatus).toBe('started');
       expect(setInterval.mock.calls.length).toBe(1);
       expect(setInterval.mock.calls[0][1]).toBe(1000);
       // Fast-forward until all timers have been executed
-      jest.runOnlyPendingTimers();
+      jest.runAllTimers();
 
       expect(countdown.state().totalSecondsToCount).toBe(0);
       expect(countdown.state().currentStatus).toBe('stopped');
